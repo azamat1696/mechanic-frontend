0322e4fd8b3c0f67d1c4f3c6207a2ad8fc073107
hal-efecan <hefecan@gmail.com>
2023-01-28 22:45:06 +0200

    performance debug

--- components/ButtonStack.js
+++ components/ButtonStack.js
@@ -1,175 +1,175 @@
-import * as React from 'react'
-import Stack from '@mui/material/Stack'
-import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
+// import * as React from 'react'
+// import Stack from '@mui/material/Stack'
+// import Box from '@mui/material/Box'
+// import Button from '@mui/material/Button'
 
-// Components
-import CreateProductForm from '../components/forms/products/CreateProductForm'
-import CreateCustomerForm from './forms/customers/CreateCustomerForm'
-import CreateSupplierForm from './forms/suppliers/CreateSupplierForm'
-import PurchaseForm from './forms/purchases/PurchaseForm'
-import CreateJobForm from './forms/orders/CreateOrderForm'
+// // Components
+// import CreateProductForm from '../components/forms/products/CreateProductForm'
+// import CreateCustomerForm from './forms/customers/CreateCustomerForm'
+// import CreateSupplierForm from './forms/suppliers/CreateSupplierForm'
+// import PurchaseForm from './forms/purchases/PurchaseForm'
+// import CreateJobForm from './forms/orders/CreateOrderForm'
 
-// Icons
-import AddIcon from '@mui/icons-material/Add'
+// // Icons
+// import AddIcon from '@mui/icons-material/Add'
 
-// Modal
-import Backdrop from '@mui/material/Backdrop'
-import Modal from '@mui/material/Modal'
-import Fade from '@mui/material/Fade'
+// // Modal
+// import Backdrop from '@mui/material/Backdrop'
+// import Modal from '@mui/material/Modal'
+// import Fade from '@mui/material/Fade'
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 750,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-}
+// const style = {
+//   position: 'absolute',
+//   top: '50%',
+//   left: '50%',
+//   transform: 'translate(-50%, -50%)',
+//   width: 750,
+//   bgcolor: 'background.paper',
+//   border: '2px solid #000',
+//   boxShadow: 24,
+//   p: 4,
+// }
 
-const ProductButton = ({ handleOpen }) => {
-  return (
-    <Button
-      endIcon={<AddIcon />}
-      onClick={handleOpen}
-      variant="contained"
-      sx={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#3c5f83',
-        '&:hover': { backgroundColor: '#3c5f83' },
-      }}
-      disableRipple={true}
-    >
-      Create
-    </Button>
-  )
-}
+// const ProductButton = ({ handleOpen }) => {
+//   return (
+//     <Button
+//       endIcon={<AddIcon />}
+//       onClick={handleOpen}
+//       variant="contained"
+//       sx={{
+//         width: '100%',
+//         height: '100%',
+//         backgroundColor: '#3c5f83',
+//         '&:hover': { backgroundColor: '#3c5f83' },
+//       }}
+//       disableRipple={true}
+//     >
+//       Create
+//     </Button>
+//   )
+// }
 
-const CustomerButton = ({ handleOpen }) => {
-  return (
-    <Button
-      endIcon={<AddIcon />}
-      onClick={handleOpen}
-      variant="contained"
-      sx={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#3c5f83',
-        '&:hover': { backgroundColor: '#3c5f83' },
-      }}
-      disableRipple={true}
-    >
-      Create
-    </Button>
-  )
-}
+// const CustomerButton = ({ handleOpen }) => {
+//   return (
+//     <Button
+//       endIcon={<AddIcon />}
+//       onClick={handleOpen}
+//       variant="contained"
+//       sx={{
+//         width: '100%',
+//         height: '100%',
+//         backgroundColor: '#3c5f83',
+//         '&:hover': { backgroundColor: '#3c5f83' },
+//       }}
+//       disableRipple={true}
+//     >
+//       Create
+//     </Button>
+//   )
+// }
 
-const SupplierButton = ({ handleOpen }) => {
-  return (
-    <Button
-      endIcon={<AddIcon />}
-      onClick={handleOpen}
-      variant="contained"
-      sx={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#3c5f83',
-        '&:hover': { backgroundColor: '#3c5f83' },
-      }}
-      disableRipple={true}
-    >
-      Supplier
-    </Button>
-  )
-}
+// const SupplierButton = ({ handleOpen }) => {
+//   return (
+//     <Button
+//       endIcon={<AddIcon />}
+//       onClick={handleOpen}
+//       variant="contained"
+//       sx={{
+//         width: '100%',
+//         height: '100%',
+//         backgroundColor: '#3c5f83',
+//         '&:hover': { backgroundColor: '#3c5f83' },
+//       }}
+//       disableRipple={true}
+//     >
+//       Supplier
+//     </Button>
+//   )
+// }
 
-const PurchaseButton = ({ handleOpen }) => {
-  return (
-    <Button
-      endIcon={<AddIcon />}
-      onClick={handleOpen}
-      variant="contained"
-      sx={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#3c5f83',
-        '&:hover': { backgroundColor: '#3c5f83' },
-      }}
-      disableRipple={true}
-    >
-      Purchase
-    </Button>
-  )
-}
+// const PurchaseButton = ({ handleOpen }) => {
+//   return (
+//     <Button
+//       endIcon={<AddIcon />}
+//       onClick={handleOpen}
+//       variant="contained"
+//       sx={{
+//         width: '100%',
+//         height: '100%',
+//         backgroundColor: '#3c5f83',
+//         '&:hover': { backgroundColor: '#3c5f83' },
+//       }}
+//       disableRipple={true}
+//     >
+//       Purchase
+//     </Button>
+//   )
+// }
 
-const OrderButton = ({ handleOpen }) => {
-  return (
-    <Button
-      endIcon={<AddIcon />}
-      onClick={handleOpen}
-      variant="contained"
-      sx={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: '#3c5f83',
-        '&:hover': { backgroundColor: '#3c5f83' },
-      }}
-      disableRipple={true}
-    >
-      Order
-    </Button>
-  )
-}
+// const OrderButton = ({ handleOpen }) => {
+//   return (
+//     <Button
+//       endIcon={<AddIcon />}
+//       onClick={handleOpen}
+//       variant="contained"
+//       sx={{
+//         width: '100%',
+//         height: '100%',
+//         backgroundColor: '#3c5f83',
+//         '&:hover': { backgroundColor: '#3c5f83' },
+//       }}
+//       disableRipple={true}
+//     >
+//       Order
+//     </Button>
+//   )
+// }
 
-export default function DividerStack({ comp, setOpen: setO }) {
-  const [open, setOpen] = React.useState(false)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+// export default function DividerStack({ comp, setOpen: setO }) {
+//   const [open, setOpen] = React.useState(false)
+//   const handleOpen = () => setOpen(true)
+//   const handleClose = () => setOpen(false)
 
-  return (
-    <Box sx={{ width: '100%', mt: 2 }}>
-      <Stack spacing={0} gap={1}>
-        {comp === 'Customers' && <CustomerButton handleOpen={handleOpen} />}
-        {comp === 'Products' && <ProductButton handleOpen={handleOpen} />}
-        {comp === 'Purchases' && <PurchaseButton handleOpen={handleOpen} />}
-        {comp === 'Suppliers' && <SupplierButton handleOpen={handleOpen} />}
-        {comp === 'Orders' && <OrderButton handleOpen={handleOpen} />}
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          open={open}
-          onClose={handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={open}>
-            <Box sx={style}>
-              {comp === 'Products' && (
-                <CreateProductForm handleClose={handleClose} />
-              )}
-              {comp === 'Customers' && (
-                <CreateCustomerForm handleClose={handleClose} />
-              )}
-              {comp === 'Suppliers' && (
-                <CreateSupplierForm handleClose={handleClose} />
-              )}
-              {comp === 'Purchases' && (
-                <PurchaseForm handleClose={handleClose} setO={setO} />
-              )}
-              {comp === 'Orders' && (
-                <CreateJobForm handleClose={handleClose} setO={setO} />
-              )}
-            </Box>
-          </Fade>
-        </Modal>
-      </Stack>
-    </Box>
-  )
-}
+//   return (
+//     <Box sx={{ width: '100%', mt: 2 }}>
+//       <Stack spacing={0} gap={1}>
+//         {comp === 'Customers' && <CustomerButton handleOpen={handleOpen} />}
+//         {comp === 'Products' && <ProductButton handleOpen={handleOpen} />}
+//         {comp === 'Purchases' && <PurchaseButton handleOpen={handleOpen} />}
+//         {comp === 'Suppliers' && <SupplierButton handleOpen={handleOpen} />}
+//         {comp === 'Orders' && <OrderButton handleOpen={handleOpen} />}
+//         <Modal
+//           aria-labelledby="transition-modal-title"
+//           aria-describedby="transition-modal-description"
+//           open={open}
+//           onClose={handleClose}
+//           closeAfterTransition
+//           BackdropComponent={Backdrop}
+//           BackdropProps={{
+//             timeout: 500,
+//           }}
+//         >
+//           <Fade in={open}>
+//             <Box sx={style}>
+//               {comp === 'Products' && (
+//                 <CreateProductForm handleClose={handleClose} />
+//               )}
+//               {comp === 'Customers' && (
+//                 <CreateCustomerForm handleClose={handleClose} />
+//               )}
+//               {comp === 'Suppliers' && (
+//                 <CreateSupplierForm handleClose={handleClose} />
+//               )}
+//               {comp === 'Purchases' && (
+//                 <PurchaseForm handleClose={handleClose} setO={setO} />
+//               )}
+//               {comp === 'Orders' && (
+//                 <CreateJobForm handleClose={handleClose} setO={setO} />
+//               )}
+//             </Box>
+//           </Fade>
+//         </Modal>
+//       </Stack>
+//     </Box>
+//   )
+// }
--- pages/test2.js
+++ pages/test2.js
@@ -1,160 +1,160 @@
-import React from 'react'
-import pdfmake from 'pdfmake/build/pdfmake'
-import pdfFonts from 'pdfmake/build/vfs_fonts'
+// import React from 'react'
+// import pdfmake from 'pdfmake/build/pdfmake'
+// import pdfFonts from 'pdfmake/build/vfs_fonts'
 
-// Hooks
-import useAuthContext from '../hooks/useAuthContext'
+// // Hooks
+// import useAuthContext from '../hooks/useAuthContext'
 
-pdfMake.vfs = pdfFonts.pdfMake.vfs
+// pdfMake.vfs = pdfFonts.pdfMake.vfs
 
-export default function Test() {
-  const { state: authState } = useAuthContext()
-  const { authToken } = authState
+// export default function Test() {
+//   const { state: authState } = useAuthContext()
+//   const { authToken } = authState
 
-  const [order, setOrder] = React.useState([])
+//   const [order, setOrder] = React.useState([])
 
-  async function fetchOrderDetail(authToken, id) {
-    const myInit = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${authToken}`,
-      },
-      mode: 'cors',
-      cache: 'default',
-      body: JSON.stringify({ orderId: id }),
-    }
-    try {
-      const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/merchants/invoice`,
-        myInit
-      )
-      const data = await res.json()
-      console.log('data', data)
-      formatData(data.orderDetail)
-      return data
-    } catch (err) {
-      return err
-    }
-  }
+//   async function fetchOrderDetail(authToken, id) {
+//     const myInit = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${authToken}`,
+//       },
+//       mode: 'cors',
+//       cache: 'default',
+//       body: JSON.stringify({ orderId: id }),
+//     }
+//     try {
+//       const res = await fetch(
+//         `${process.env.NEXT_PUBLIC_BASE_URL}/merchants/invoice`,
+//         myInit
+//       )
+//       const data = await res.json()
+//       console.log('data', data)
+//       formatData(data.orderDetail)
+//       return data
+//     } catch (err) {
+//       return err
+//     }
+//   }
 
-  function formatData(data) {
-    const arrData = data.map((d) => {
-      const product = {
-        Name: d.product.name,
-        Quantity: d.quantity,
-        Price: d.price,
-        Total: d.quantity * d.price,
-      }
-      return product
-    })
-    setOrder(arrData)
-  }
+//   function formatData(data) {
+//     const arrData = data.map((d) => {
+//       const product = {
+//         Name: d.product.name,
+//         Quantity: d.quantity,
+//         Price: d.price,
+//         Total: d.quantity * d.price,
+//       }
+//       return product
+//     })
+//     setOrder(arrData)
+//   }
 
-  const columns = ['Name', 'Quantity', 'Price', 'Total']
+//   const columns = ['Name', 'Quantity', 'Price', 'Total']
 
-  function buildTableBody(data, columns) {
-    const body = []
-    body.push(columns)
+//   function buildTableBody(data, columns) {
+//     const body = []
+//     body.push(columns)
 
-    data.forEach(function (row) {
-      const dataRow = []
-      columns.forEach(function (column) {
-        dataRow.push(row[column].toString())
-      })
-      body.push(dataRow)
-    })
+//     data.forEach(function (row) {
+//       const dataRow = []
+//       columns.forEach(function (column) {
+//         dataRow.push(row[column].toString())
+//       })
+//       body.push(dataRow)
+//     })
 
-    return body
-  }
+//     return body
+//   }
 
-  function table(data, columns) {
-    return {
-      headerRows: 1,
-      widths: [125, 65, 65, 65],
-      body: buildTableBody(data, columns),
-    }
-  }
+//   function table(data, columns) {
+//     return {
+//       headerRows: 1,
+//       widths: [125, 65, 65, 65],
+//       body: buildTableBody(data, columns),
+//     }
+//   }
 
-  let docDef = {
-    content: [
-      {
-        columns: [
-          {
-            width: '65%',
-            text: 'Invoice',
-            style: 'header',
-            margin: [0, 2, 10, 20],
-          },
-          {
-            width: '35%',
-            stack: ['Address A', 'Address B', 'Address C'],
-            fontSize: 12,
-            style: 'align',
-          },
-        ],
-      },
-      { table: table(order, columns), style: 'table' },
-    ],
-    styles: {
-      header: {
-        fontSize: 40,
-        bold: true,
-      },
-      table: {
-        fontSize: 12,
-      },
-      align: {
-        alignment: 'right',
-      },
-    },
-  }
+//   let docDef = {
+//     content: [
+//       {
+//         columns: [
+//           {
+//             width: '65%',
+//             text: 'Invoice',
+//             style: 'header',
+//             margin: [0, 2, 10, 20],
+//           },
+//           {
+//             width: '35%',
+//             stack: ['Address A', 'Address B', 'Address C'],
+//             fontSize: 12,
+//             style: 'align',
+//           },
+//         ],
+//       },
+//       { table: table(order, columns), style: 'table' },
+//     ],
+//     styles: {
+//       header: {
+//         fontSize: 40,
+//         bold: true,
+//       },
+//       table: {
+//         fontSize: 12,
+//       },
+//       align: {
+//         alignment: 'right',
+//       },
+//     },
+//   }
 
-  const dwnldPdf = () => pdfMake.createPdf(docDef).open()
+//   const dwnldPdf = () => pdfMake.createPdf(docDef).open()
 
-  React.useEffect(() => {
-    console.log('order', order)
-    if (order.length !== 0) {
-      dwnldPdf()
-    }
-  }, [order])
+//   React.useEffect(() => {
+//     console.log('order', order)
+//     if (order.length !== 0) {
+//       dwnldPdf()
+//     }
+//   }, [order])
 
-  return (
-    <div style={divStyle}>
-      <button
-        style={btnStyle}
-        onClick={() => {
-          setOrder([])
-          fetchOrderDetail(authToken, 220)
-        }}
-      >
-        220
-      </button>
-      <button
-        style={btnStyle}
-        onClick={() => {
-          setOrder([])
-          fetchOrderDetail(authToken, 226)
-        }}
-      >
-        226
-      </button>
+//   return (
+//     <div style={divStyle}>
+//       <button
+//         style={btnStyle}
+//         onClick={() => {
+//           setOrder([])
+//           fetchOrderDetail(authToken, 220)
+//         }}
+//       >
+//         220
+//       </button>
+//       <button
+//         style={btnStyle}
+//         onClick={() => {
+//           setOrder([])
+//           fetchOrderDetail(authToken, 226)
+//         }}
+//       >
+//         226
+//       </button>
 
-      <button onClick={dwnldPdf} style={btnStyle}>
-        Open Pdf
-      </button>
-    </div>
-  )
-}
+//       <button onClick={dwnldPdf} style={btnStyle}>
+//         Open Pdf
+//       </button>
+//     </div>
+//   )
+// }
 
-const divStyle = {
-  margin: '30px 0 0 30px',
-  display: 'flex',
-  flexDirection: 'column',
-  width: '200px',
-  height: '100px',
-}
+// const divStyle = {
+//   margin: '30px 0 0 30px',
+//   display: 'flex',
+//   flexDirection: 'column',
+//   width: '200px',
+//   height: '100px',
+// }
 
-const btnStyle = {
-  margin: '10px 0 0 0',
-}
+// const btnStyle = {
+//   margin: '10px 0 0 0',
+// }
